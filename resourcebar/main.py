76742613ad364080bfa3d40af0d047c237d8d232
victor-rene <victor-rene.dev@outlook.com>
2015-01-11 15:32:02 +0100

    Improvements to resourcebar, padding, slant is variable and animatable.

--- resourcebar/main.py
+++ resourcebar/main.py
@@ -7,10 +7,13 @@ from resourcebar import ResourceBar
 
 if __name__ == '__main__':
   layout = RelativeLayout()
-  rb = ResourceBar(slanted=True, size_hint=(.6, .2),
-    pos_hint={'center_x': .45, 'center_y': .5})
+  rb = ResourceBar(size_hint=(.6, .2),
+    pos_hint={'center_x': .5, 'center_y': .5})
   layout.add_widget(rb)
-  anim = Animation(fill_pct=1) + Animation(fill_pct=0)
-  anim.repeat = True
-  anim.start(rb)
+  anim1 = Animation(fill_pct=1.) + Animation(fill_pct=0.)
+  anim1.repeat = True
+  anim1.start(rb)
+  anim2 = Animation(slant_pct=-1.) + Animation(slant_pct=1.)
+  anim2.repeat = True
+  anim2.start(rb)
   runTouchApp(layout)
--- resourcebar/resourcebar.py
+++ resourcebar/resourcebar.py
@@ -1,18 +1,22 @@
 from kivy.graphics import Color, Quad
-from kivy.properties import BoundedNumericProperty
+from kivy.properties import BoundedNumericProperty, NumericProperty
 from kivy.uix.widget import Widget
 
 
 class ResourceBar(Widget):
 
-  fill_pct = BoundedNumericProperty(.5, min=0, max=1)
+  slant_pct = BoundedNumericProperty(.0, min=-1., max=1.)
+  fill_pct = BoundedNumericProperty(.0, min=0., max=1.)
+  min_hue = BoundedNumericProperty(.0, min=0., max=1.)
+  max_hue = BoundedNumericProperty(1./3, min=0., max=1.)
+  padding_x = NumericProperty(5.)
+  padding_y = NumericProperty(0.)
 
   def __init__(self, **kw):
     super(ResourceBar, self).__init__(**kw)
-    self.slanted = kw['slanted'] if 'slanted' in kw else False
-    self.fill_pct = 0.
-    self.min_hue = kw['min_hue'] if 'min_hue' in kw else 0
-    self.max_hue = kw['max_hue'] if 'max_hue' in kw else 1./3
+    for k, v in kw.iteritems():
+        if hasattr(self, k):
+            setattr(self, k, v)
     with self.canvas.before:
       self.quadbg_color = Color(.1, .4, .4, 1)
       self.quadbg = Quad(source='img/resourcebar_bg.png')
@@ -25,32 +29,40 @@ class ResourceBar(Widget):
     self.bind(pos=self._update)
     self.bind(size=self._update)
     self.bind(fill_pct=self._update_fill_pct)
+    self.bind(slant_pct=self._update_slant_pct)
     
   def _update_fill_pct(self, *args):
-    """ Set the percentage of the bar between 0 et 1."""
+    """ Set the fill percentage of the bar (between 0 et 1). """
     self._update_quad(self.quad, self.fill_pct)
     self._update_fill_hue(self.fill_pct)
     
+  def _update_slant_pct(self, *args):
+    """ Set the fill percentage of the bar (between 0 et 1). """
+    self._update_quad(self.quadbg, 1.)
+    self._update_quad(self.quadfg, 1.)
+    
   def _update(self, *args):
-    self.slant = self.height if self.slanted else 0
+    """ Update all quads. """
     self._update_quad(self.quadbg, 1)
     self._update_quad(self.quad, self.fill_pct)
     self._update_quad(self.quadfg, 1)
     
   def _update_quad(self, quad, pct):
-    slant = self.height if self.slanted else 0
-    x1 = self.x
-    y1 = self.y
-    x2 = x1 + self.slant
-    y2 = self.y + self.height
-    x3 = x2 + self.width * pct
+    """ Update one qued. """
+    padding_x = self.padding_x if quad == self.quad else 0.
+    padding_y = self.padding_y if quad == self.quad else 0.
+    x1 = self.x + padding_x
+    y1 = self.y + padding_y
+    x2 = x1 + (self.height * self.slant_pct) + padding_x
+    y2 = self.y + self.height - (padding_y * 2)
+    x3 = x2 + (self.width * pct) - (padding_x * 2)
     y3 = y2
-    x4 = x1 + self.width * pct
+    x4 = x1 + (self.width * pct) - (padding_x * 2)
     y4 = y1
     quad.points = [x1, y1, x2, y2, x3, y3, x4, y4]
     
   def _update_fill_hue(self, fill_pct):
-    """ Set the percentage of the bar between 0 et 1."""
+    """ Set the color of the bar. """
     h = (self.max_hue - self.min_hue) * fill_pct + self.min_hue
     s = .8
     v = .8
